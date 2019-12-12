e8fea073c6e3ef31e1d4d54164d9ccf39a4c4a9c
matan <57196433+spyroy@users.noreply.github.com>
2019-12-12 23:23:21 +0000

    add main

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "Ex3_.h"
+#define size 50
 
 //main function
 int main(int argc, char **argv)
@@ -12,12 +13,30 @@ int main(int argc, char **argv)
 		printf("%d,",arr[i]);
 	}
 	printf("\n");
-	int arr2[6] = {10,7,11,54,2,1};
-	insertion_sort(arr2,5);
+	int arr2[8] = {10,7,11,54,2,1,12,3};
+	insertion_sort(arr2,8);
 	int j;
-	for(j = 0; j < 6; j++)
+	for(j = 0; j < 8; j++)
 	{
 		printf("%d,",arr2[j]);
 	}
 	printf("\n");
+
+	int k;
+	int num;
+	int user[size] = {0};
+	int* user_ptr = user;
+	for(k = 0; k < size; k++)
+	{
+		scanf(" %d",&num);
+		*user_ptr = num;
+		user_ptr++;
+	}
+	insertion_sort(user,size); 
+	int l;
+	for(l = 0; l < size; l++)
+	{
+		printf("%d,",user[l]);
+	}
+	printf("\n");	
 }
